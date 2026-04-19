d059eb4398e744345b8fca1a3576250e1ae2d603
PrekshaSbabu <84118268+PrekshaSbabu@users.noreply.github.com>
2026-04-19 13:27:58 +0530

    Update remove_ibm.py

--- rules/remove_ibm.py
+++ rules/remove_ibm.py
@@ -1,1 +1,10 @@
-
+def remove_ibm_extensions(obj):
+    if isinstance(obj, dict):
+        return {
+            k: remove_ibm_extensions(v)
+            for k, v in obj.items()
+            if not k.startswith("x-ibm-")
+        }
+    elif isinstance(obj, list):
+        return [remove_ibm_extensions(i) for i in obj]
+    return obj
