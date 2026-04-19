3181200483c45e404bf9a8f41f09123a012f53c8
PrekshaSbabu <84118268+PrekshaSbabu@users.noreply.github.com>
2026-04-19 13:24:06 +0530

    Update core.py

--- transformer/core.py
+++ transformer/core.py
@@ -1,6 +1,6 @@
-from .rules.remove_ibm import remove_ibm_extensions
-from .rules.normalize import normalize_host_basepath
-from .rules.inject_integration import inject_integration
+from rules.remove_ibm import remove_ibm_extensions
+from rules.normalize import normalize_host_basepath
+from rules.inject_integration import inject_integration
 
 def transform_spec(spec):
     spec = remove_ibm_extensions(spec)
