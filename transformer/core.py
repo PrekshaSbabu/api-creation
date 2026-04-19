ba9cbbf8dfa1ff7c019ee2a92930746b277067c0
PrekshaSbabu <84118268+PrekshaSbabu@users.noreply.github.com>
2026-04-19 13:29:50 +0530

    Update core.py

--- transformer/core.py
+++ transformer/core.py
@@ -3,7 +3,10 @@ from rules.normalize import normalize_host_basepath
 from rules.inject_integration import inject_integration
 
 def transform_spec(spec):
-    spec = remove_ibm_extensions(spec)
+    # extract backend FIRST
     spec = normalize_host_basepath(spec)
     spec = inject_integration(spec)
+
+    # THEN remove ibm
+    spec = remove_ibm_extensions(spec)
     return spec
